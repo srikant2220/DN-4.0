2d200aa28ae4543435f6f8c7aaf88d52f3698943
srikant2220 <srikantpitta2004@gmail.com>
2025-06-18 20:04:39 +0530

    Create word.java

---
+++ Mandatory Hands-on(Design Patterns and Principles)/FactoryMethodPatternExample/word.java
@@ -0,0 +1,7 @@
+package FactoryMethodPatternExample;
+public class word implements WordDocument {
+    public void storedata()
+    {
+        System.out.println("Storing in Word Document");
+    }
+}
