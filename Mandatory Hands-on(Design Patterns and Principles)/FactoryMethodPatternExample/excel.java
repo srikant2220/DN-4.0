6a9dd666d3dd90a17c530accfe228f1c892cc1f6
srikant2220 <srikantpitta2004@gmail.com>
2025-06-18 20:05:18 +0530

    Create excel.java

---
+++ Mandatory Hands-on(Design Patterns and Principles)/FactoryMethodPatternExample/excel.java
@@ -0,0 +1,7 @@
+package FactoryMethodPatternExample;
+public class excel implements ExcelDocument{
+    public void storedata()
+    {
+        System.out.println("Storing in excel document");
+    }
+}
